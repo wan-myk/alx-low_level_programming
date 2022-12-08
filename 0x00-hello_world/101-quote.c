4cddf644b174a73158c7dce59d75227a384d3ece
wan-myk <mykle231@gmail.com>
2022-12-08 15:52:52 -0800

    Advanced task 2-Last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -11,7 +12,7 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 }
