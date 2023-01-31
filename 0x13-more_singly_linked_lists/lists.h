5ba3f7421ebfbab21d681ff8c09e3f06483b8820
wan-myk <mykle231@gmail.com>
2023-01-31 13:48:31 -0800

    Header file updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -16,8 +16,8 @@
 
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+	int n;
+	struct listint_s *next;
 }
 listint_t;
 
@@ -37,5 +37,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
-
 #endif
